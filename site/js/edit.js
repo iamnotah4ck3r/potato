615f9f73599ac1c7c0c3c41227f55f836037d9f1
Luke I <96824126+iamnotah4ck3r@users.noreply.github.com>
2022-01-28 12:11:27 -0300

    Initialize gh-pages

---
+++ site/js/edit.js
@@ -0,0 +1,40 @@
+/* 
+
+ - Declarar una variable -
+var edad;
+
+ - Inicializar - 
+edad = 30;
+
+ - Objeto -
+var persona = {
+	nombre: "Jorge",
+	edad: 30
+}
+
+ - String -
+var elementos = ["Juego", "Manos"]
+
+ - Función -
+// Declarativas: 
+
+function myFunction() {
+	return 3;
+}
+
+// Expresión:
+
+var myFunction = function(a,b) {
+	return a + b;
+}
+
+ Ejemplo:
+
+function suma(num1, num2){
+   var resultado = num1 + num2;
+   return resultado;
+}
+
+*/
+
+
